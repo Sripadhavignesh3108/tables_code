315dae24d30d4c514a4b3c53973a55b415e1977f
Sripadhavignesh3108 <sripadhavignesh@gmail.com>
2024-06-28 21:04:24 +0530

    table code

---
+++ tables_code.js
@@ -0,0 +1,14 @@
+
+//creating a js code for a maths tables maker
+
+const prompt = require("prompt-sync")();
+let a=prompt("enter a number :");
+// let a=prompt();
+for(i=1;i<=10;i++)
+{
+    console.log(a+"x"+i+"="+i*a)
+};
+
+
+
+
